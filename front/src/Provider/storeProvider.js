0ba3352f83792af637398bd2af99362dac6366d9
Leonardo <odriozola-leo@hotmail.com>
2022-02-23 15:54:24 -0300

    front desacoplado,mejora en la estructura de directorios del proyecto

---
+++ front/src/Provider/storeProvider.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from "react";
+import storeReducer from '../Reducer/storeReducer';
+const initialState = {
+    todo: {
+        list: [],
+        item: {} 
+        }
+};
+
+const Store = createContext(initialState)
+
+const StoreProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(storeReducer, initialState);
+  
+    return(
+        <Store.Provider value={{ state, dispatch }}>
+        {children}
+    </Store.Provider>
+    )
+  
+}
+
+export { Store };
+export default StoreProvider;
